ae1dae52a5a1588a663d4a2588115b20791cd67c
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-10-31 14:02:06 +0900

    Title: 짝수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241031/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int b, a;
+    cin >> b >> a;
+    int i = b;
+    while (i >= a)
+    {
+        if ( i % 2 == 0)
+        {
+            cout << i << " ";
+        }
+        i--;
+    }
+    return 0;
+}
