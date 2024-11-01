707fbbe7e98e51381fbdc34ead1a27394b9313f1
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-01 13:30:51 +0900

    Title: 대각선으로 숫자 채우기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241101/대각선으로 숫자 채우기/diagonal-numbering.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int arr[100][100];
+int num = 1;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	int square = n + m - 2;
+	for (int k = 0; k < square; k++)
+	{
+		for (int j = 0; j <= k; j++)
+		{
+			if (j >= n) continue;
+			if (k - j >= m) continue;
+			arr[j][k - j] = num;
+			num++;
+			
+		}
+	}
+	arr[n - 1][m - 1] = num;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+		return 0;
+}
