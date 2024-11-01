17d7e2427e70b12d3f67a168c5b2d22aa519f66f
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-01 14:53:43 +0900

    Title: 가장 짧은 문자열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241101/가장 짧은 문자열/shortest-string.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a, b, c;
+
+    cin >> a >> b >> c;
+
+    int a_leng = a.length();
+    int b_leng = b.length();
+    int c_leng = c.length();
+    
+    int a1 = a_leng - b_leng;
+    int b1 = b_leng - c_leng;
+    int c1 = c_leng - a_leng;
+
+    if ( a1 < 0 ) a1 = a1 * (-1);
+    if ( b1 < 0 ) b1 = b1 * (-1);
+    if ( c1 < 0 ) c1 = c1 * (-1);
+
+    if ( a1 > b1 && a1 > c1) cout << a1;
+    else if (b1 > a1 && b1 > c1 ) cout << b1;
+    else cout << c1;
+    return 0;
+}
