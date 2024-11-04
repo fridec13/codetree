196497be4812386425bbd2d0e102d383f8d58e41
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-04 17:59:30 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241104/재귀함수를 이용한 3n + 1 수열/3n-plus-1-sequence-with-recursive-function.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int cnt = 0;
+
+int F(int n)
+{
+	if (n == 1)
+	{
+		return 0;
+	}
+
+	if (n % 2 == 1)
+	{
+		return F(n * 3 + 1) + 1;
+	}
+	else if (n % 2 == 0)
+	{
+		return F(n / 2) + 1;
+	}
+}
+
+int main() {
+	// 여기에 코드를 작성해주세요.
+	int n;
+	cin >> n;
+	cout << F(n);
+	return 0;
+}
