9471e0284308c628d3061fe5b266fdcc17fad68e
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-04 13:59:02 +0900

    Title: 절댓값으로 변경, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241104/절댓값으로 변경/find-the-absolute-value.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int arr[50];
+
+void MakeAbs(int arr[], int i)
+{
+    if (arr[i] < 0)
+    {
+        arr[i] = arr[i] * (-1);
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        MakeAbs(arr, i);
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
