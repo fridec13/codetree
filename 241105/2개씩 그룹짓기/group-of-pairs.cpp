503be862173336b21c1f7e0cf5a0f775467a4c94
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-05 11:41:37 +0900

    Title: 2개씩 그룹짓기, Time: 16ms, Memory: 0MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241105/2개씩 그룹짓기/group-of-pairs.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[1000];
+int arr2[1000];
+int arr3[1000];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int N;
+    cin >> N;
+    for (int i = 0; i < 2*N; i++)
+    {
+        cin >> arr[i];
+        arr2[i] = arr[i];
+    }
+    sort(arr, arr + 2*N);
+    sort(arr2, arr2 + 2*N, greater<int>());
+    
+    int maxval = 0;
+
+    for (int i = 0; i < 2*N; i++)
+    {
+        int val = arr[i] + arr2[i];
+        if (val >= maxval)
+        {
+            maxval = val;
+        }
+    }
+
+    cout << maxval;
+
+    return 0;
+}
