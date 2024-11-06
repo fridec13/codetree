aee32c7697438dfae9628e612490030f32c501a1
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-06 09:23:56 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241106/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a >> b >> c;
+    int cur = 11 * 24 * 60 + 11 * 60 + 11;
+    int goal = a * 24 * 60 + b * 60 + c;
+
+    cout << goal - cur;
+
+
+    return 0;
+}
