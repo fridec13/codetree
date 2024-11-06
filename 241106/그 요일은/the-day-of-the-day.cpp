d48e24596d623a9c2cc57224b772a409fabcb837
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-06 10:14:57 +0900

    Title: 그 요일은, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241106/그 요일은/the-day-of-the-day.cpp
+++ 241106/그 요일은/the-day-of-the-day.cpp
@@ -3,62 +3,65 @@
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    int date[13] = {
-        0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
-    };
+	// 여기에 코드를 작성해주세요.
+	int date[13] = {
+		0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+	};
 
-    string yoil_list[7] = {
-        "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"
-    };
+	string yoil_list[7] = {
+		"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"
+	};
 
-    int m1, d1, m2, d2;
-    cin >> m1 >> d1 >> m2 >> d2;
-    string yoil_goal;
-    cin >> yoil_goal;
+	int m1, d1, m2, d2;
+	cin >> m1 >> d1 >> m2 >> d2;
+	string yoil_goal;
+	cin >> yoil_goal;
 
-    int goalyoilidx = 0;
-    for (int i = 0; i < 7; i++)
-    {
-        if (yoil_list[i] == yoil_goal)
-        {
-            goalyoilidx = i;
-        }
-    }
-    int month = m1;
-    int day = d1;
-    int cntyoil = 0;
-    int yoil = 1;
+	int goalyoilidx = 0;
+	for (int i = 0; i < 7; i++)
+	{
+		if (yoil_list[i] == yoil_goal)
+		{
+			goalyoilidx = i;
+		}
+	}
+	int month = m1;
+	int day = d1;
+	int cntyoil = 0;
+	int yoil = 1;
 
-    while (true)
-    {
-        if (yoil > 6)
-        {
-            yoil = 0;
-        }
+	while (true)
+	{
 
-        if (yoil_list[yoil] == yoil_list[goalyoilidx])
-        {
-            cntyoil++;
-        }
+		if (month == m2 && day == d2)
+		{
+			cout << cntyoil;
+			break;
+		}
 
-        if (month == m2 && day == d2)
-        {
-            cout << cntyoil -1;
-            break;
-        }
-        
-        if (day > date[month])
-        {
-            month++;
-            day = 1;
-            continue;
-        }
-        
-        day++;
-        yoil++;
+		if (yoil > 6)
+		{
+			yoil = 0;
+		}
 
 
-    }
-    return 0;
+
+		if (day > date[month])
+		{
+			month++;
+			day = 1;
+			continue;
+		}
+
+		if (yoil == goalyoilidx)
+		{
+			cntyoil++;
+		}
+
+		day++;
+		yoil++;
+
+
+	}
+	return 0;
 }
