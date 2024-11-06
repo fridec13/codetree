07ebc64a899b7f28e6fad4686057e556f79c9684
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-06 11:39:17 +0900

    Title: 최대로 겹치는 지점, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241106/최대로 겹치는 지점/maximum-overlapped-points.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[100];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int s, e;
+        cin >> s >> e;
+        for (int j = s; j <= e; j++)
+        {
+            arr[j]++;
+        }
+    }
+    int maxval = -9999;
+    for (int i =0; i < 100; i++)
+    {
+        maxval = max(maxval, arr[i])
+    }
+
+    cout << maxval;
+    return 0;
+}
