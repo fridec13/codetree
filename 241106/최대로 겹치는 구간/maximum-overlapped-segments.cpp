0c74d9d317153462e698493742d70f60c0f8b927
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-06 11:36:30 +0900

    Title: 최대로 겹치는 구간, Time: 13ms, Memory: 0MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241106/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int arr[100];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int cnt = 1;
+    for (int i =0; i < n; i++)
+    {
+        int flag = 0;
+        int s, e;
+        cin >> s >> e;
+        for (int j = s; j < e; j++)
+        {
+            arr[j]++;
+            if (arr[j] > cnt)
+            {
+                flag = 1;
+            }
+        }
+        if (flag == 1)
+        {
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
