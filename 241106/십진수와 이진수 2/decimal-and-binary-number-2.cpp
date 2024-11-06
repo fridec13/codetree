4290ee98ccc3eea85522ee988eacded778faf1aa
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-06 10:52:25 +0900

    Title: 십진수와 이진수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241106/십진수와 이진수 2/decimal-and-binary-number-2.cpp
+++ 241106/십진수와 이진수 2/decimal-and-binary-number-2.cpp
@@ -3,39 +3,38 @@
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    // string N;
-    // cin >> N;
-    int num = 0;
-    while (true)
-    {
-        int binary1 = cin.get() - '0';
-        if (cin.eof()) break;
-        num = num * 2 + binary1;
-    }
+	// 여기에 코드를 작성해주세요.
+	string N;
+	cin >> N;
+	int num = 0;
+	for (int i = 0; i < N.length(); i++)
+	{
+		int a = N[i] - '0';
+		num = num * 2 + a;
+	}
 
-    num = num * 17;
-
-
-    int binary[1000] = {};
-    int cnt = 0;
-    while (true)
-    {
-        if (num < 2)
-        {
-            binary[cnt] = num % 2;
-            break;
-        }
-
-        binary[cnt] = num % 2;
-        cnt++;
-        num = num/2;
-    }
-    for (int i = cnt; i >= 0; i--)
-    {
-        cout << binary[i];
-    }
+	num = num * 17;
 
 
-    return 0;
+	int binary[1000] = {};
+	int cnt = 0;
+	while (true)
+	{
+		if (num < 2)
+		{
+			binary[cnt] = num % 2;
+			break;
+		}
+
+		binary[cnt] = num % 2;
+		cnt++;
+		num = num / 2;
+	}
+	for (int i = cnt; i >= 0; i--)
+	{
+		cout << binary[i];
+	}
+
+
+	return 0;
 }
