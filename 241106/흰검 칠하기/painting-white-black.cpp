4cd1089fcfce87f9faea3668c2a92e83c122224f
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-06 17:31:22 +0900

    Title: 흰검 칠하기, Time: 48ms, Memory: 5MB, Status: Passed - CodeTree

--- 241106/흰검 칠하기/painting-white-black.cpp
+++ 241106/흰검 칠하기/painting-white-black.cpp
@@ -1,51 +1,37 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
-int arr[10000][3];
+map<int, array<int, 3>> arr;
 
 int main() {
-	// 여기에 코드를 작성해주세요.
-
 	int cur = 5000; // 오프셋
 	int n;
 	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; i++) {
 		int dist;
 		char dir;
-		cin >> dist;
-		cin >> dir;
+		cin >> dist >> dir;
 		dist = dist - 1;
 
-		if (dir == 'L')
-		{
-			for (int i = cur; i >= cur - dist; i--)
-			{
+		if (dir == 'L') {
+			for (int i = cur; i >= cur - dist; i--) {
 				arr[i][0]++;
 				arr[i][2] = 1;
-				if (arr[i][1] >= 2 && arr[i][0] >= 2)
-				{
+				if (arr[i][1] >= 2 && arr[i][0] >= 2) {
 					arr[i][2] = 3;
 				}
 			}
 			cur = cur - dist;
-		}
-
-		else if (dir == 'R')
-		{
-
-			for (int i = cur; i <= cur + dist; i++)
-			{
+		} else if (dir == 'R') {
+			for (int i = cur; i <= cur + dist; i++) {
 				arr[i][1]++;
 				arr[i][2] = 2;
-				if (arr[i][1] >= 2 && arr[i][0] >= 2)
-				{
+				if (arr[i][1] >= 2 && arr[i][0] >= 2) {
 					arr[i][2] = 3;
 				}
-				
 			}
 			cur = cur + dist;
-			
 		}
 	}
 
@@ -53,11 +39,10 @@ int main() {
 	int cntb = 0;
 	int cntg = 0;
 
-	for (int i = 0; i < 10000; i++)
-	{
-		if (arr[i][2] == 1) cntw++;
-		else if (arr[i][2] == 2) cntb++;
-		else if (arr[i][2] == 3) cntg++;
+	for (auto& elem : arr) {
+		if (elem.second[2] == 1) cntw++;
+		else if (elem.second[2] == 2) cntb++;
+		else if (elem.second[2] == 3) cntg++;
 	}
 
 	cout << cntw << " " << cntb << " " << cntg;
