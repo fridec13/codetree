73c9decffd2f7a2fee62137d87c909c7ba90f775
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-06 14:10:32 +0900

    Title: 흰검 칠하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241106/흰검 칠하기/painting-white-black.cpp
+++ 241106/흰검 칠하기/painting-white-black.cpp
@@ -8,68 +8,125 @@ int arr[10000][3];
 //0번 타일 부터 시작해서
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    int n;
-    cin >> n;
-    int fredist = 5000;
+	// 여기에 코드를 작성해주세요.
+	int n;
+	cin >> n;
+	int fredist = 5000;
+	char fredir = ' ';
 
-    for (int i = 0; i < n; i++)
-    {
-        int dist;
-        cin >> dist;
-        char dir;
-        cin >> dir;
-        if (dir == 'R')
-        {
-            for (int i = fredist; i < fredist + dist; i++)
-            {
-                arr[i][0]++;
+	for (int i = 0; i < n; i++)
+	{
+		int dist;
+		cin >> dist;
+		char dir;
+		cin >> dir;
+		if (dir == 'R')
+		{
+			if (dist == 1)
+			{
+				arr[fredist][0]++;
 
-                if (arr[i][0] >= 2 && arr[i][1] >= 2)
-                {
-                    arr[i][2] = 1;
-                    continue;
-                }
-                arr[i][2] = 3;
+				if (arr[fredist][0] >= 2 && arr[fredist][1] >= 2)
+				{
+					arr[fredist][2] = 1;
+					continue;
+				}
 
-            }
-            fredist = fredist + dist;
-        }
-        else
-        {
-            for (int i = fredist -1; i >= fredist - dist; i--)
-            {
-                arr[i][1]++;
+				arr[fredist][2] = 3;
+			}
+			else
+			{
+				for (int i = fredist; i < fredist + dist; i++)
+				{
+					arr[i][0]++;
 
-                if (arr[i][0] >= 2 && arr[i][1] >= 2)
-                {
-                    arr[i][2] = 1;
-                    continue;
-                }
-                
-                arr[i][2] = 2;
-            }
-            fredist = fredist - dist;
-        }
-    }
-    int cntgrey =0;
-    int cntwhite =0;
-    int cntblack = 0;
-    for (int i = 0; i < 10000; i++)
-    {
-        if (arr[i][2] == 1)
-        {
-            cntgrey++;
-        }
-        else if (arr[i][2] == 2)
-        {
-            cntwhite++;
-        }
-        else if (arr[i][2] == 3)
-        {
-            cntblack++;
-        }
-    }
-    cout << cntwhite << " " << cntblack << " " << cntgrey;
-    return 0;
+					if (arr[i][0] >= 2 && arr[i][1] >= 2)
+					{
+						arr[i][2] = 1;
+						continue;
+					}
+					arr[i][2] = 3;
+
+				}
+
+				if (dist > 1)
+				{
+					fredist = fredist + dist;
+					fredir = 'R';
+				}
+			}
+		}
+		else
+		{
+			if (dist == 1)
+			{
+				if (fredir == 'R')
+				{
+					fredist = fredist - 1;
+					arr[fredist][1]++;
+
+					if (arr[fredist][0] >= 2 && arr[fredist][1] >= 2)
+					{
+						arr[fredist][2] = 1;
+						continue;
+					}
+
+					arr[fredist][2] = 2;
+					continue;
+				}
+				arr[fredist][1]++;
+
+				if (arr[fredist][0] >= 2 && arr[fredist][1] >= 2)
+				{
+					arr[fredist][2] = 1;
+					continue;
+				}
+
+				arr[fredist][2] = 2;
+				continue;
+			}
+
+			else
+			{
+				for (int i = fredist - dist; i < fredist; i++)
+				{
+					arr[i][1]++;
+
+					if (arr[i][0] >= 2 && arr[i][1] >= 2)
+					{
+						arr[i][2] = 1;
+						continue;
+					}
+
+					arr[i][2] = 2;
+				}
+
+				if (dist > 1)
+				{
+					fredist = fredist - dist;
+					fredir = 'L';
+				}
+			}
+		}
+	}
+	int cntgrey = 0;
+	int cntwhite = 0;
+	int cntblack = 0;
+	for (int i = 0; i < 10000; i++)
+	{
+		if (arr[i][2] == 1)
+		{
+			cntgrey++;
+		}
+		else if (arr[i][2] == 2)
+		{
+			cntwhite++;
+		}
+		else if (arr[i][2] == 3)
+		{
+			cntblack++;
+		}
+	}
+	cout << cntwhite << " " << cntblack << " " << cntgrey;
+	return 0;
 }
