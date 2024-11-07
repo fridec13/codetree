a66ae6eaa184574f1a08026312306f148e4617cf
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-07 11:17:09 +0900

    Title: 만나는 그 순간, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241107/만나는 그 순간/the-moment-we-meet.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int A[10000];
+int B[10000];
+int cur = 5000;
+
+int main() {
+	// 여기에 코드를 작성해주세요.
+
+	int N, M;
+	cin >> N >> M;
+	int t = 0;
+
+	for (int i = 0; i < N; i++)
+	{
+		char dir;
+		int dist;
+		cin >> dir >> dist;
+		// dist = dist - 1;
+		if (dir == 'R')
+		{
+			for (int j = cur; j < cur + dist; j++)
+			{
+				A[t] = j - 5000;
+				t++;
+			}
+			cur = cur + dist;
+		}
+		else if (dir == 'L')
+		{
+			for (int j = cur; j > cur - dist; j--)
+			{
+				A[t] = j - 5000;
+				t++;
+			}
+			cur = cur - dist;
+		}
+	}
+
+	t = 0;
+	cur = 5000;
+
+	for (int i = 0; i < M; i++)
+	{
+		char dir;
+		int dist;
+		cin >> dir >> dist;
+		if (dir == 'R')
+		{
+			for (int j = cur; j < cur + dist; j++)
+			{
+				B[t] = j - 5000;
+				t++;
+			}
+			cur = cur + dist;
+		}
+		else if (dir == 'L')
+		{
+			for (int j = cur; j > cur - dist; j--)
+			{
+				B[t] = j-5000;
+				t++;
+			}
+			cur = cur - dist;
+		}
+
+	}
+	int flag = 0;
+
+	for (int i = 0; i < 10000; i++)
+	{
+		//cout << "time" << i << ":" << A[i] << " " << B[i] << "\n";
+		if (A[i] == B[i] && A[i] > 0 && B[i] > 0)
+		{
+			cout << i;
+			return 0;
+		}
+	}
+	 //for (int i = 0; i < 10000; i++)
+	 //{
+	 //    if (A[i] == B[i])
+	 //    {
+	 //        flag = i;
+	 //        break;
+	 //    }
+	 //}
+	 //int d = 1;
+
+	 //(flag) ? cout << flag : cout << -1; 
+	cout << -1;
+	return 0;
+}
