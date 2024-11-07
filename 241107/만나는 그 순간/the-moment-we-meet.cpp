a15289bcf0c1f98bdbd0a272ee2e35219ae9afe8
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-07 11:23:21 +0900

    Title: 만나는 그 순간, Time: 16ms, Memory: 0MB, Status: Memory Limit Exceed - CodeTree

--- 241107/만나는 그 순간/the-moment-we-meet.cpp
+++ 241107/만나는 그 순간/the-moment-we-meet.cpp
@@ -6,6 +6,8 @@ using namespace std;
 int A[10000];
 int B[10000];
 int cur = 5000;
+int Aalldist;
+int Balldist;
 
 int main() {
 	// 여기에 코드를 작성해주세요.
@@ -19,6 +21,7 @@ int main() {
 		char dir;
 		int dist;
 		cin >> dir >> dist;
+		Aalldist = Aalldist + dist;
 		// dist = dist - 1;
 		if (dir == 'R')
 		{
@@ -48,6 +51,7 @@ int main() {
 		char dir;
 		int dist;
 		cin >> dir >> dist;
+		Balldist = Balldist + dist;
 		if (dir == 'R')
 		{
 			for (int j = cur; j < cur + dist; j++)
@@ -61,7 +65,7 @@ int main() {
 		{
 			for (int j = cur; j > cur - dist; j--)
 			{
-				B[t] = j-5000;
+				B[t] = j - 5000;
 				t++;
 			}
 			cur = cur - dist;
@@ -70,7 +74,7 @@ int main() {
 	}
 	int flag = 0;
 
-	for (int i = 0; i <= 1000; i++)
+	for (int i = 0; i < max(Aalldist, Balldist); i++)
 	{
 		//cout << "time" << i << ":" << A[i] << " " << B[i] << "\n";
 		if (A[i] == B[i] && i != 0)
@@ -79,17 +83,17 @@ int main() {
 			return 0;
 		}
 	}
-	 //for (int i = 0; i < 10000; i++)
-	 //{
-	 //    if (A[i] == B[i])
-	 //    {
-	 //        flag = i;
-	 //        break;
-	 //    }
-	 //}
-	 //int d = 1;
+	//for (int i = 0; i < 10000; i++)
+	//{
+	//    if (A[i] == B[i])
+	//    {
+	//        flag = i;
+	//        break;
+	//    }
+	//}
+	//int d = 1;
 
-	 //(flag) ? cout << flag : cout << -1; 
+	//(flag) ? cout << flag : cout << -1; 
 	cout << -1;
 	return 0;
 }
