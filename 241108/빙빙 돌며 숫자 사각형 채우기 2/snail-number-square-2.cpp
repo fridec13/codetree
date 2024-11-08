d6c8c63cb8d06880a8140c32c9f4f55ff00ea116
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-08 10:32:03 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241108/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+
+int map[100][100];
+
+int dy[4] = {1, 0, -1, 0};
+int dx[4] = {0, 1, 0, -1};
+
+bool InRange(int ny, int nx)
+{
+    return (0 <= ny && ny < n && 0 <= nx && nx < m);
+}
+
+int rotdir(int curdir)
+{
+    return (curdir + 1) % 4;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m;
+    map[0][0] = 1;
+
+    int y = 0;
+    int x = 0;
+    int curdir = 0;
+    for (int i = 2; i <= n*m; i++)
+    {
+        int ny = y + dy[curdir];
+        int nx = x + dx[curdir];
+        if ( InRange(ny, nx) && map[ny][nx] == 0)
+        {
+            y = ny;
+            x = nx;
+            map[y][x] = i;
+        }
+        else if (!InRange(ny, nx) || map[ny][nx] > 0)
+        {
+            curdir = rotdir(curdir);
+            ny = y + dy[curdir];
+            nx = x + dx[curdir];
+            if (InRange(ny, nx) && map[ny][nx] == 0)
+            {
+                y = ny;
+                x = nx;
+                map[y][x] = i;
+            }
+        }
+    }
+
+    for (int i =0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << map[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
