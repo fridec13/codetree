ed8168e7163f40f1b6ca1be4a368ad2707dbcf6f
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-10 21:38:16 +0900

    Title: 최고의 13위치 2, Time: 1045ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 241110/최고의 13위치 2/best-place-of-13-2.cpp
+++ 241110/최고의 13위치 2/best-place-of-13-2.cpp
@@ -14,7 +14,7 @@ bool InRange(int y, int x)
 int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n;
-    int inputcheck;
+    int inputcheck = 0;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
