bf0de20be6165188c72afebca65fd8f91575838b
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-11 16:26:36 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 29ms, Memory: 0MB, Status: Passed - CodeTree

--- 241111/두 가지로 열리는 자물쇠/a-two-way-lock.cpp
+++ 241111/두 가지로 열리는 자물쇠/a-two-way-lock.cpp
@@ -12,51 +12,9 @@ int key2[3];
 // 원형 자물쇠임 // 나머지 연산
 int circular(int key, int j) 
 {
-    int diff[4] = {0};
-    diff[0] = -1;
-    diff[1] = -1;
-    diff[2] = -1;
-    diff[3] = -1;
-
-    for (int i = 0; i < n; i++) 
-    {
-        if (j + i == key) 
-        {
-            diff[0] = i;
-            break;
-        }
-        if ((j + i) % n == key) 
-        {
-            diff[1] = i;
-            break;
-        }
-    }
-
-    for (int i = 0; i < n; i++) 
-    {
-        if (j - i == key) 
-        {
-            diff[2] = i;
-            break;
-        }
-        if ((j - i + n) % n == key) 
-        { // 음수 나머지 방지
-            diff[3] = i;
-            break;
-        }
-    }
-
-    // diff 배열에서 유효한 값의 최소값을 찾음
-    int minval = n; // 초기값을 충분히 큰 값으로 설정
-    for (int i = 0; i < 4; i++) 
-    {
-        if (diff[i] != -1) 
-        {
-            minval = min(minval, diff[i]);
-        }
-    }
-
-    return minval;
+    int right = (key - j + n) % n;
+    int left = (j - key + n) % n;
+    return min(right, left);
 }
 
 int main() 
@@ -87,14 +45,10 @@ int main()
                 int key22 = circular(key2[1], j);
                 int key23 = circular(key2[2], k);
 
-                if (key11 <= 2 && key12 <= 2 && key13 <= 2) 
+                if (key11 <= 2 && key12 <= 2 && key13 <= 2 || key21 <= 2 && key22 <= 2 && key23 <= 2) 
                 {
                     cnt++;
                 } 
-                else if (key21 <= 2 && key22 <= 2 && key23 <= 2) 
-                {
-                    cnt++;
-                }
             }
         }
     }
