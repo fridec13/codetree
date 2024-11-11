ef5932bc75b43eb637a6e8e910c653e146d2806c
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-11 11:34:02 +0900

    Title: 아름다운 수열 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241111/아름다운 수열 2/beautiful-sequence-2.cpp
+++ 241111/아름다운 수열 2/beautiful-sequence-2.cpp
@@ -20,16 +20,16 @@ int main() {
         cin >> B[i];
     }
     int cnt = 0;
-    for (int i = 0; i <= n-m; i++) // A의 시작점 선택
+    for (int i = 0; i <= n-m -1; i++) // A의 시작점 선택 38 37 35까지 i가 돌아야함. 39 - 3 36임. 
     {
         int check[100]= {0};
         int flag = 0;
-        for (int j = i; j < i+m; j++) // j는 시작점 부터 +m까지 확인.
+        for (int j = i; j <= i+m-1; j++) // j는 시작점 부터 +m까지 확인.0, 1, 2까지
         {
             // 확인할 숫자 하나.
-            for (int k = 0; k < m; k++)
+            for (int k = 0; k < m; k++) // B[0] 2 B[1] 5 B[2] 2;
             {
-                if (check[k] == 1) continue; 
+                if (check[k] == 1) continue; //이미 체크가 된 거면 패스,
                 if (A[j] == B[k] ) // 고른 숫자가 B[0]과 같다면 0을 1로 바꿈, 다음으로 고른 숫자가 B[2] 와 같다면 check[2] = 1로 바꿈.
                 {
                     check[k]++;// 다시 생각해야함.
