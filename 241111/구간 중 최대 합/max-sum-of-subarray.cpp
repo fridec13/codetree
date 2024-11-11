249546f61605685365667ac4bd2b5d1ee5d9c4c2
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-11 10:23:06 +0900

    Title: 구간 중 최대 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241111/구간 중 최대 합/max-sum-of-subarray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[100];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n, k;
+    cin >> n >> k;
+    int maxval = -1;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i <= n-k; i++)
+    {
+        int sum = 0;
+        for (int j = i; j < i+3; j++)
+        {
+            sum += arr[j];
+        }
+        maxval = max(maxval, sum);
+    }
+    cout << maxval;
+    return 0;
+}
