1333907a977a4ee0ca227db91f96aa03d07508ac
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-11 10:39:28 +0900

    Title: G or H 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241111/G or H 3/G-or-H-3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+char alpha[10000];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, k;
+    cin >> n >> k;
+    // 0 5 =  k == 5; 
+    for (int i = 0; i < n; i++)
+    {
+        int idx;
+        char gliph;
+        cin >> idx >> gliph;
+        alpha[idx] = gliph;
+    }
+
+    int maxval = -1;
+
+    for (int i = 1; i <= n-k+1; i++)
+    {
+        int sum= 0;
+        for (int j = i; j <= i+k; j++)
+        {
+            if (alpha[j] == 'G')
+            {
+                sum += 1;
+            }
+            else if ( alpha[j] == 'H')
+            {
+                sum += 2;
+            }
+        }
+        maxval = max(maxval, sum);
+    }
+    cout << maxval;
+
+    return 0;
+}
