f7dabeba2bdaf68ad900bc8a51d99a98318246a8
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-13 17:44:13 +0900

    Title: 상해버린 치즈, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241113/상해버린 치즈/rotten-cheese.cpp
+++ 241113/상해버린 치즈/rotten-cheese.cpp
@@ -1,102 +1,68 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-struct People
+struct People 
 {
     int eattime;
     int illtime;
 };
 
-struct Cheese
+struct Cheese 
 {
-    People peoplelist[100];
+    People peoplelist[101];
     bool mayberotten;
 };
 
 Cheese Cheeselist[51];
 
-int checkill[51];
-
-int main() {
-    // 여기에 코드를 작성해주세요.
+int main() 
+{
     int n, m, d, s;
     cin >> n >> m >> d >> s;
     
-    for (int i =0; i < d; i++)
+    for (int i = 0; i < d; i++) 
     {
         int p, m, t;
         cin >> p >> m >> t;
         Cheeselist[m].peoplelist[p].eattime = t;
     }
 
-    for (int i = 0; i < s; i++)
+    for (int i = 0; i < s; i++) 
     {
         int p, t;
         cin >> p >> t;
-        for (int j = 0; j < m; j++)
+        Cheeselist[0].peoplelist[p].illtime = t;
+    }
+
+    int maxSick = 0;
+    for (int i = 1; i <= m; i++) 
+    {
+        vector<int> sickPeople;
+        for (int j = 1; j <= n; j++) 
         {
-            if ( Cheeselist[j].peoplelist[p].eattime > 0)
+            if (Cheeselist[i].peoplelist[j].eattime > 0 && 
+                Cheeselist[0].peoplelist[j].illtime > Cheeselist[i].peoplelist[j].eattime) 
             {
-                Cheeselist[j].peoplelist[p].illtime = t;
+                sickPeople.push_back(j);
             }
+        }
+        
+        if (sickPeople.size() == s)
+        {
+            int count = 0;
+            for (int j = 1; j <= n; j++) 
+            {
+                if (Cheeselist[i].peoplelist[j].eattime > 0) 
+                {
+                    count++;
+                }
+            }
+            maxSick = max(maxSick, count);
         }
     }
 
-    for (int i =1; i <= m; i++) // 상한 치즈는 단하나임.
-    {
-        bool flag  = true;
-        for (int j = 1; j <= n; j++)
-        {
-            if (Cheeselist[i].peoplelist[j].eattime == 0 ) continue;
-
-            else if (Cheeselist[i].peoplelist[j].eattime > 0 && Cheeselist[i].peoplelist[j].illtime > 0)
-            {
-                if (Cheeselist[i].peoplelist[j].eattime >= Cheeselist[i].peoplelist[j].illtime)
-                {
-                    continue;
-                }
-                else if (Cheeselist[i].peoplelist[j].illtime >= Cheeselist[i].peoplelist[j].eattime)
-                {
-                    Cheeselist[i].mayberotten = true;
-                }
-            }
-        }
-    }
-    int cntcheese[51] = {};
-    for (int i = 1; i <= m; i++)
-    {
-        // if ( i == 1)
-        // {
-        //     for (int j = 1; j <= n; j++)
-        //     {
-        //         if ( Cheeselist[i].peoplelist[j].eattime > 0)
-        //         {
-        //             cout << i << " " << j << "\n";
-        //         }
-        //     }
-        // }
-        if (Cheeselist[i].mayberotten)
-        {
-            for (int j = 1; j <= n; j++)
-            {
-                if ( Cheeselist[i].peoplelist[j].eattime > 0)
-                {
-                    // cout << i << j << " ";
-                    cntcheese[i]++;
-                }
-            }
-        }
-    }
-
-
-    // cout << "\n";
-    int maxcnt = 0;
-    for (int i =1; i <= m; i++)
-    {
-        maxcnt = max(maxcnt, cntcheese[i]);
-    }
-
-    cout << maxcnt;
-    
+    cout << maxSick;
     return 0;
 }
