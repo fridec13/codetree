7fde3d0f66cc83c928cc6baa5a42b10735c581b3
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-18 14:44:54 +0900

    Title: 스승의 은혜 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241118/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+
+//선물하나를 정해서 반값할인
+//최대명수 출력
+struct Student
+{
+    int price;
+    int sentpee;
+};
+
+Student arr[1000];
+
+bool cmp(Student a, Student b)
+{
+    return a.price + a.sentpee < b.price + b.sentpee;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int b;
+    cin >> n >> b;
+    int allsum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i].price >> arr[i].sentpee;
+        allsum = allsum + arr[i].price + arr[i].sentpee;
+    }
+
+    sort(arr, arr+n, cmp);
+
+    if (b >= allsum)
+    {
+        cout << n;
+        return 0;
+    }
+
+    int maxval = -1;
+
+    for (int i = 0; i < n; i++) // 할인 제품 고르기
+    {
+        arr[i].price = arr[i].price / 2;
+        int sum = 0;
+        int cnt = 0;
+        for (int j = 0; j < n; j++) // 확인
+        {
+            sum = sum + arr[j].price + arr[j].sentpee;
+            if (sum > b) break;
+            cnt++;
+        }
+        maxval = max(cnt, maxval);
+        arr[i].price = arr[i].price * 2;
+        
+    }
+
+    cout << maxval;
+    return 0;
+}
