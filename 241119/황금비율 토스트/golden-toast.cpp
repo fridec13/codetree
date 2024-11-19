efdaa6409a228f5b97c6fda0efb411f7ff6a341a
fridec13 <59463154+fridec13@users.noreply.github.com>
2024-11-19 12:20:51 +0900

    Title: 황금비율 토스트, Time: 16ms, Memory: 1MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241119/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+list<char> l;
+list<char>::iterator it;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+    
+    for (int i= 0; i < n; i++)
+    {
+        char input;
+        cin >> input;
+        l.push_back(input);
+    }
+    it = l.end();
+    for (int i = 0; i < m; i++)
+    {
+        char cmd;
+        cin >> cmd;
+        if (cmd == 'L')
+        {
+            it--;
+        }
+        if (cmd == 'P')
+        {
+            char insertch;
+            cin >> insertch;
+            l.insert(it, insertch);
+        }
+        if (cmd == 'R')
+        {
+            it++;
+        }
+        if (cmd == 'D')
+        {
+            l.erase(it);
+        }
+
+    }
+
+    for (it = l.begin(); it != l.end(); it++)
+    {
+        cout << *it;
+    }
+    return 0;
+}
